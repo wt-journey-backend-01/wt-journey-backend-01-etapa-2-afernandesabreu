6be40ea86c19f5c241b7a5812556edadc490d0b4
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-22 17:26:00 -0300

    importando módulo de rotas e definindo rota para agenteRoutes e casosRoutes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+const agenteRoutes = require('./routes/agentesRoutes');
+const casosRoutes = require('./routes/casosRoutes');
 const PORT = 3000;
 
 app.use(express.json());
+app.use('/agentes', agenteRoutes);
+app.use('/casos', casosRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Servidor do Departamento de Polícia rodando em localhost:${PORT}`);
