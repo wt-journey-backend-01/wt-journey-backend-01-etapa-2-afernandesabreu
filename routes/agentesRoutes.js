4cce557cf8b38a800d84f44d9695c5472c7ece3f
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-22 16:24:03 -0300

    adicionando rota para agentes

---
+++ routes/agentesRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getAllAgentes, getAgenteById, createAgente, updateAgente, deleteAgent } = require('../controllers/agentesController');
+
+router.get('/', getAllAgentes);
+router.get('/:id', getAgenteById);
+router.post('/', createAgente);
+router.put('/:id', updateAgente);
+router.patch('/:id', patchAgente);
+router.delete('/:id', deleteAgent);
+
+module.exports = router;
